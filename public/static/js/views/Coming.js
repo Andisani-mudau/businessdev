a9fc9e536f0569ad5cafe893ec9ee7321428eef0
andisani-mudau <andisanimudau101@gmail.com>
2024-08-25 15:42:26 +0200

    updated atuff

--- public/static/js/views/Coming.js
+++ public/static/js/views/Coming.js
@@ -15,6 +15,7 @@ export default class extends AbstractView {
                 <img src="https:/\/illustrations.popsy.co/amber/page-under-construction.svg" alt="">
                 </div>
                 <h1 style="">Coming Soon!</h1>
+                <p>Please leave the kitchen, we are still cooking.</p>
             </div>
             <div class="copy">
                 <p>&copy; 2024 businessdev. All rights reserved.</p>
