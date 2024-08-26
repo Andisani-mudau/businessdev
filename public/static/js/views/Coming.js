6246ac5be54545218758314146a2d90e2fef7189
andisani-mudau <andisanimudau101@gmail.com>
2024-08-26 11:03:09 +0200

    updated index.css

--- public/static/js/views/Coming.js
+++ public/static/js/views/Coming.js
@@ -10,17 +10,16 @@ export default class extends AbstractView {
         console.log("wejjaaaaj");
         return `
             <section class="coming">
-            <div class="banner-container illu">
+            <div class="illu">
                 <div class="illustration2">
                 <img src="https:/\/illustrations.popsy.co/amber/page-under-construction.svg" alt="">
                 </div>
-                <h1 style="">Coming Soon!</h1>
+                <h1 style="soon">Coming Soon!</h1>
                 <p>Please leave the kitchen, we are still cooking.</p>
             </div>
             <div class="copy">
                 <p>&copy; 2024 businessdev. All rights reserved.</p>
             </div>
-            
         </section>
         `;
     }
